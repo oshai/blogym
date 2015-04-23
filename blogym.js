d7a408fae90c7bcc2a42530d6b72ea4de3c8706e
oshai <ohadshai@gmail.com>
2015-04-23 23:07:27 +0300

    formatting and file seperation

--- blogym.js
+++ blogym.js
@@ -1,54 +1,53 @@
-
 Blogs = new Mongo.Collection("blogs");
 
 Router.configure({
-  layoutTemplate: 'ApplicationLayout'
+    layoutTemplate: 'ApplicationLayout'
 });
-Router.route('/', function () {
-  this.render('blogListTemplate');
+Router.route('/', function() {
+    this.render('blogListTemplate');
 });
-Router.route('/blog/:_id', function () {
-  var blog = Blogs.findOne({_id: this.params._id});
-  this.render('showBlog', {data: blog});
+Router.route('/blog/:_id', function() {
+    var blog = Blogs.findOne({_id: this.params._id});
+    this.render('showBlog', {data: blog});
 });
 
 
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.blogListTemplate.helpers({
-    blogs: function () {
-      return Blogs.find({}, {sort: {createdAt: -1}, limit: 10});
-    }
-  });
-  
-  Template.blogListTemplate.events({
-  "submit .new-blog": function (event) {
-    // This function is called when the new blog form is submitted
-
-    var title = event.target.title.value;
-	var email = event.target.email.value;
-	var text = event.target.text.value;
-
-    Blogs.insert({
-      title: title,
-	  email: email,
-	  text: text,
-      createdAt: new Date() // current time
+    // This code only runs on the client
+    Template.blogListTemplate.helpers({
+        blogs: function() {
+            return Blogs.find({}, {sort: {createdAt: -1}, limit: 10});
+        }
     });
 
-    // Clear form
-    event.target.title.value = "";
-	event.target.email.value = "";
-	event.target.text.value = "";
+    Template.blogListTemplate.events({
+        "submit .new-blog": function(event) {
+            // This function is called when the new blog form is submitted
 
-    // Prevent default form submit
-    return false;
-  }
-});
+            var title = event.target.title.value;
+            var email = event.target.email.value;
+            var text = event.target.text.value;
 
-Template.blog.events({
-  "click .delete": function () {
-    Blogs.remove(this._id);
-  }
-});
+            Blogs.insert({
+                title: title,
+                email: email,
+                text: text,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.title.value = "";
+            event.target.email.value = "";
+            event.target.text.value = "";
+
+            // Prevent default form submit
+            return false;
+        }
+    });
+
+    Template.blog.events({
+        "click .delete": function() {
+            Blogs.remove(this._id);
+        }
+    });
 }
